e4c0d3567f48fa4b5a998d880806c3af21f3e540
NathanRS <jonathan15rojas@gmail.com>
2018-04-25 20:44:19 +0200

    Prova pue de pagina

--- peupagina.php
+++ peupagina.php
@@ -4,16 +4,27 @@ Plugin Name: Peu de pàgina per defecte per a noves entrades
 Plugin URI: Escriu l'adreça URL completa del teu plugin dins de Github
 Description: Crea peus de pàgina per defecte per a noves entrades
 Version: 0.1
-Author: El teu nom i cognom
-Author URI: L'adreça URL del teu compte de Github
+Author: Jonathan Rojas
+Author URI: https://github.com/jorosa1718daw2/pluginwp2_jorosa
 License:  GNU GPLv3 
 */
 function admin_menu_peu_pagina(){
-	$titolPagina="Peu de pagina per defecte";
-	$titolEntrada="Peru de pagina per defecte";
+	$titolPagina="Peu de pàgina per defecte";
+	$titolEntrada="Peu de pàgina per defecte";
 	$role="administrator";
 	$id="peu_pagina_per_defecte";
-	add_menu_page($titoPagina,$titolEntrada,$role,$id);
+	$nom_funcio="nouPeuPagDef";
+	add_menu_page($titolPagina,$titolEntrada,$role,$id,$nom_funcio); 
+}
+function nouPeuPagDef(){
+	include_once('formPeuPag.php');
+}
+function afegPeuPagDef(){
+    echo "<div style='color: red;
+    font-size: 10px;
+    margin: 10px;'>".get_option('footer_text')."</div>";
 }
 add_action('admin_menu','admin_menu_peu_pagina');
+add_action('wp_footer','afegPeuPagDef');
+?>
 ?>
